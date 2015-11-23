bf1234040418e37ea2308c8b0ac4b44071cf4c62
Brent Buchholz <brentbuchholz@gmail.com>
2015-11-23 13:30:42 -0600

    added more image types to MnemoSyne package

--- app/src/main/java/com/nineworldsdeep/gauntlet/mnemosyne/MnemoSyneUtils.java
+++ app/src/main/java/com/nineworldsdeep/gauntlet/mnemosyne/MnemoSyneUtils.java
@@ -4,21 +4,22 @@ import com.nineworldsdeep.gauntlet.Utils;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 /**
  * Created by brent on 11/16/15.
  */
 public class MnemoSyneUtils {
+
+    private static String[] imageExts = {"png", "jpg", "gif"}; //TODO: add more formats here
+
     public static List<String> getImages(File dir) {
 
         List<String> lst = new ArrayList<>();
 
         lst.addAll(Utils.getAllDirectoryNames(dir));
 
-        String[] exts = {"png"}; //TODO: add more formats here
-        lst.addAll(Utils.getAllFileNamesWithExt(dir, exts));
+        lst.addAll(Utils.getAllFileNamesWithExt(dir, imageExts));
 
         return lst;
     }
@@ -29,8 +30,7 @@ public class MnemoSyneUtils {
 
         lst.addAll(getImageListItemsFromPaths(Utils.getAllDirectoryPaths(dir)));
 
-        String[] exts = {"png"}; //TODO: add more formats here
-        lst.addAll(getImageListItemsFromPaths(Utils.getAllFilePathsWithExt(dir, exts)));
+        lst.addAll(getImageListItemsFromPaths(Utils.getAllFilePathsWithExt(dir, imageExts)));
 
         return lst;
     }

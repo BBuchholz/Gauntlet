bd43d12199b6141ee9de65acb0ae7d8718306a27
Brent Buchholz <brentbuchholz@gmail.com>
2015-11-25 16:35:18 -0600

    working on #37 - audio player, got the basic play functionality working on load, buttons still inactive

--- app/src/main/java/com/nineworldsdeep/gauntlet/mnemosyne/AudioDisplayActivity.java
+++ app/src/main/java/com/nineworldsdeep/gauntlet/mnemosyne/AudioDisplayActivity.java
@@ -1,6 +1,8 @@
 package com.nineworldsdeep.gauntlet.mnemosyne;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,9 +13,12 @@ import android.view.View;
 import com.nineworldsdeep.gauntlet.R;
 import com.nineworldsdeep.gauntlet.Utils;
 
+import java.io.IOException;
+
 public class AudioDisplayActivity extends AppCompatActivity {
 
     private FileListItem ili;
+    //private MediaPlayer mp;
 
     public static final String EXTRA_AUDIOPATH =
             "com.nineworldsdeep.gauntlet.AUDIODISPLAY_AUDIO_PATH";
@@ -35,6 +40,19 @@ public class AudioDisplayActivity extends AppCompatActivity {
             ili = new FileListItem(s);
 
             Utils.toast(this, ili.toString());
+
+            //mp = MediaPlayer.create(this, Uri.parse(ili.getFile().getPath()));
+            //mp.start();
+
+            MediaPlayerSingleton mps = MediaPlayerSingleton.getInstance();
+
+            try {
+                mps.play(ili.getFile().getPath());
+
+            } catch (IOException e) {
+
+                Utils.toast(this, e.getMessage());
+            }
 
         }else{
 
---
+++ app/src/main/java/com/nineworldsdeep/gauntlet/mnemosyne/MediaPlayerSingleton.java
@@ -0,0 +1,47 @@
+package com.nineworldsdeep.gauntlet.mnemosyne;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+import java.io.IOException;
+
+/**
+ * Created by brent on 11/25/15.
+ */
+public class MediaPlayerSingleton {
+
+    private static MediaPlayerSingleton singleton = null;
+
+    private MediaPlayer mp;
+
+    private MediaPlayerSingleton(){
+        //singleton constructor
+        mp = new MediaPlayer();
+
+    }
+
+    public static MediaPlayerSingleton getInstance(){
+
+        if(singleton == null){
+            singleton = new MediaPlayerSingleton();
+        }
+
+        return singleton;
+    }
+
+    public void play(String path) throws IOException {
+
+        if(mp.isPlaying()){
+            mp.stop();
+
+            //TODO: this is a hack
+            //it keeps crashing when I try to change
+            //sources, so I'm just creating a new one
+            mp = new MediaPlayer();
+        }
+
+        mp.setDataSource(path);
+        mp.prepare();
+        mp.start();
+    }
+}

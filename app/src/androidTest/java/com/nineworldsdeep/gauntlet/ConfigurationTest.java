f86d387f10b4045e07ad954fbd16ce737fabb0fc
Brent Buchholz <brentbuchholz@gmail.com>
2015-12-31 12:42:34 -0600

    Add stub tests for configuration

---
+++ app/src/androidTest/java/com/nineworldsdeep/gauntlet/ConfigurationTest.java
@@ -0,0 +1,59 @@
+package com.nineworldsdeep.gauntlet;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by brent on 12/31/15.
+ */
+public class ConfigurationTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSynergyDirectory() throws Exception {
+        fail("prototype");
+    }
+
+    @Test
+    public void testGetArchiveDirectory() throws Exception {
+        fail("prototype");
+    }
+
+    @Test
+    public void testGetTemplateDirectory() throws Exception {
+        fail("prototype");
+    }
+
+    @Test
+    public void testGetConfigDirectory() throws Exception {
+        fail("prototype");
+    }
+
+    @Test
+    public void testGetAllSynergyListNames() throws Exception {
+        fail("prototype");
+    }
+
+    @Test
+    public void testGetAllTemplateListNames() throws Exception {
+        fail("prototype");
+    }
+
+    @Test
+    public void testGetAllArchiveListNames() throws Exception {
+        fail("prototype");
+    }
+
+}

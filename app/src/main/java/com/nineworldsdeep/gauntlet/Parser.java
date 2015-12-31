0ef87eb687449ae391108163e3d27045d87a99fe
Brent Buchholz <brentbuchholz@gmail.com>
2015-12-31 12:17:40 -0600

    Add methods to parser for support of atomic and mixed content (preparation for Synergy V3)

--- app/src/main/java/com/nineworldsdeep/gauntlet/Parser.java
+++ app/src/main/java/com/nineworldsdeep/gauntlet/Parser.java
@@ -3,6 +3,7 @@ package com.nineworldsdeep.gauntlet;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
+import java.util.regex.Pattern;
 
 /**
  * Created by brent on 11/30/15.
@@ -153,5 +154,129 @@ public class Parser {
         }
 
         return openBracesCount == closingBracesCount;
+    }
+
+    //NEW METHODS 2015-12-30
+    public boolean isAtomic(String input) {
+        String temp = input;
+
+        while(temp.length() > 0 && startsWithKeyValTag(temp))
+        {
+            String key = getFirstKey(temp);
+            temp = trimKeyVal(key, temp);
+        }
+
+        return temp.trim().length() == 0;
+    }
+
+    public String trimLastKeyVal(String key, String input) {
+
+        String value = extractLastOne(key, input);
+        String keyVal = key + "={" + value + "}";
+        String temp = replaceLast(input, Pattern.quote(keyVal), " ");
+
+        //clear any double spaces that result
+        while (temp.contains("  "))
+        {
+            temp = temp.replace("  ", " ");
+        }
+
+        return temp.trim();
+    }
+
+    public String replaceLast(String text, String regex, String replacement) {
+        //FROM: http://stackoverflow.com/questions/2282728/java-replacelast
+        return text.replaceFirst("(?s)" + regex + "(?!.*?" + regex + ")", replacement);
+    }
+
+    public String trimKeyVal(String key, String input) {
+
+        String value = extract(key, input);
+        String keyVal = key + "={" + value + "}";
+        String temp = input.replaceFirst(Pattern.quote(keyVal), " ");
+
+        //clear any double spaces that result
+        while(temp.contains("  "))
+        {
+            temp = temp.replace("  ", " ");
+        }
+
+        return temp.trim();
+    }
+
+    public String getFirstKey(String input) {
+
+        if (!input.contains("={"))
+        {
+            return null;
+        }
+
+//        String firstSegment = input.Split(new string[] { "={" },
+//            StringSplitOptions.None)[0];
+
+        String firstSegment = input.split("=\\{")[0];
+
+        String[] words = firstSegment.split(" ");
+        String lastWord = words[words.length - 1];
+
+        return lastWord.trim();
+    }
+
+    public boolean startsWithKeyValTag(String input) {
+
+        String trimmed = input.trim();
+
+        if (trimmed.indexOf("={") < 0)
+        {
+            return false;
+        }
+
+        if (trimmed.indexOf(" ") < 0)
+        {
+            return true;
+        }
+
+        return trimmed.indexOf("={") < trimmed.indexOf(" ");
+    }
+
+    private String extractLastOne(String key, String input) {
+
+        String openKey = key + "={";
+
+        int startIndex = input.lastIndexOf(openKey);
+
+        if(startIndex > -1){
+
+            int innerContentStartIndex =
+                    startIndex + openKey.length();
+            int innerContentEndIndex =
+                    input.length() - 1; //just a temp value
+
+            int nestingLevel = 0;
+            boolean closed = false;
+
+            for(int i = startIndex; !closed; i++){
+
+                if(Character.compare('{', input.charAt(i)) == 0){
+
+                    nestingLevel++;
+                }
+
+                if(Character.compare('}', input.charAt(i)) == 0){
+
+                    nestingLevel--;
+
+                    if(nestingLevel == 0){
+                        closed = true;
+                        innerContentEndIndex = i;
+                    }
+                }
+            }
+
+            return input.substring(innerContentStartIndex,
+                    innerContentEndIndex);
+        }
+
+        return null;
     }
 }

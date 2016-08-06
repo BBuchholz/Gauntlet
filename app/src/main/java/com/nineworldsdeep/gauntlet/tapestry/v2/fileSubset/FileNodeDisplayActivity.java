85fabec2c5bb97ae6ea5fcb99cd66e02d61d226a
Brent Buchholz <brentbuchholz@gmail.com>
2016-08-06 18:42:47 -0500

    started working on node model

---
+++ app/src/main/java/com/nineworldsdeep/gauntlet/tapestry/v2/fileSubset/FileNodeDisplayActivity.java
@@ -0,0 +1,15 @@
+package com.nineworldsdeep.gauntlet.tapestry.v2.fileSubset;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nineworldsdeep.gauntlet.R;
+
+public class FileNodeDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_node_display);
+    }
+}

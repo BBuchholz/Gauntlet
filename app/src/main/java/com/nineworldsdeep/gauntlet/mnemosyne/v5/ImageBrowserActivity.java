9ddfc3b856c058da152eec786c59e83b3b999752
Brent Buchholz <brentbuchholz@gmail.com>
2017-01-13 18:28:23 -0600

    mini-commit: empty tabs

--- app/src/main/java/com/nineworldsdeep/gauntlet/mnemosyne/v5/ImageBrowserActivity.java
+++ app/src/main/java/com/nineworldsdeep/gauntlet/mnemosyne/v5/ImageBrowserActivity.java
@@ -1,7 +1,9 @@
 package com.nineworldsdeep.gauntlet.mnemosyne.v5;
 
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.nineworldsdeep.gauntlet.R;
 
@@ -12,5 +14,13 @@ public class ImageBrowserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_browser);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("Tab 1"));
+        tabs.addTab(tabs.newTab().setText("Tab 2"));
+        tabs.addTab(tabs.newTab().setText("Tab 3"));
     }
 }
